3bacbe1aa2f5763f7663a4c6b73c4296d2b34d45
Matthew Lee <matthewlee686@gmail.com>
2021-08-02 10:39:24 -0400

    Add Junit Test for Game Entity

--- MockGameStore/src/test/java/com/skilldistillery/gamestore/MockGameStoreApplicationTests.java
+++ MockGameStore/src/test/java/com/skilldistillery/gamestore/MockGameStoreApplicationTests.java
@@ -1,13 +1,36 @@
 package com.skilldistillery.gamestore;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.skilldistillery.gamestore.entities.Game;
+import com.skilldistillery.gamestore.repositories.GameRepository;
 
 @SpringBootTest
 class MockGameStoreApplicationTests {
 
+	@Autowired
+	private GameRepository gameRepo; 
+	
 	@Test
-	void contextLoads() {
+	@DisplayName("Find Game by Id")
+	void test1() {
+		Optional<Game> g = gameRepo.findById(1);
+		if(g.isPresent()) {
+			assertNotNull(g);
+			assertEquals(1, g.get().getId());
+		} else {
+			fail("No Game Found");
+		}
+			
 	}
 
 }
