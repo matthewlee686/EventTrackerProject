53cf79325fbb3ffc82e3b2443fd4e77988694ca6
Matthew Lee <matthewlee686@gmail.com>
2021-08-16 13:19:11 -0400

    Configure ng app for production deployment

--- ngMockGameStore/src/app/services/game.service.ts
+++ ngMockGameStore/src/app/services/game.service.ts
@@ -3,14 +3,15 @@ import { Injectable } from '@angular/core';
 import { Observable, throwError } from 'rxjs';
 import { Game } from '../models/game';
 import { catchError } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GameService {
 
-  baseUrl = 'http://localhost:8090/';
-  url = this.baseUrl + 'api/games';
+  // baseUrl = 'http://localhost:8090/';
+  url = environment.baseUrl + 'api/games';
 
   constructor(
     private http : HttpClient
